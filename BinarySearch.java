34df1f4db7bdba88e72311aba97095f5fc4c0686
eCurrington <eli.currington@stu.daviess.kyschools.us>
2025-04-16 14:09:48 +0000

    done

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,8 +2,39 @@ import java.util.Arrays;
 
 public class BinarySearch
 {
+	
+
+
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+		
+		int bottom = 0;
+		int top = ray.length-1;
+		int middle;
+		while(bottom <= top)
+		{
 
+			middle =  (bottom+top) / 2;
+			//System.out.println(" " + middle);
+
+			if(ray[middle] < item)
+			{
+				bottom = middle+1;
+			}
+
+			else if(ray[middle] > item)
+			{
+				
+				top = middle-1;
+			}
+			else
+			{
+				return middle;
+			}
+		}
+		//System.out.println(" " + top);
+		//System.out.println(" " + bottom);
+		return -1;
 	}
 }
